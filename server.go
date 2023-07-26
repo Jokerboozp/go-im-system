59f989fd08335730d8a172ae41c66228128d5a05
jokerboozp <2929590482@qq.com>
2023-07-26 15:29:15 +0800

    构建基础server

---
+++ server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+}
+
+// 创建一个server的接口
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:   ip,
+		Port: port,
+	}
+	return server
+}
+
+func (s *Server) Handler(conn net.Conn) {
+	fmt.Println("链接建立成功")
+}
+
+// 启动服务器的接口
+func (s *Server) Start() {
+	// socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.Ip, s.Port))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer listener.Close()
+
+	for {
+		// accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		//do handler
+		go s.Handler(conn)
+	}
+}
